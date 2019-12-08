72c77e75c8ce09afa454101f5e6468c29ca55a26
Sean Hellum <seanhellum45@gmail.com>
2019-12-08 21:14:56 +0000

    fix server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-app.use(express.static('static'));
+app.use(express.static('/static'));
 app.get('/', (req, res) => {
-    res.render('main.html');
+    res.sendFile("/workspace/Risk2.0/static/main.html");
 })
 app.listen(3000);
 console.log("Server running on port 3000\nlocalhost:3000");
