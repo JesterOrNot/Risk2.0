bdd8ec3054a8992a671b2cd35445fdba2f514df9
Sean Hellum <seanhellum45@gmail.com>
2019-12-09 00:08:08 +0000

    make app portable

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require("path")
 const app = express();
 app.use(express.static('/static'));
 app.get('/', (req, res) => {
-    res.sendFile("/workspace/Risk2.0/static/main.html");
+    res.sendFile(__dirname + "/static/main.html");
 })
 app.listen(3000);
 console.log("Server running on port 3000\nlocalhost:3000");
